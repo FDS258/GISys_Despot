cb6619bc485ace60728607b840f2fea6515f3cb5
FDS258 <67225400+FDS258@users.noreply.github.com>
2023-03-12 13:41:20 +0800

    Add files via upload

---
+++ Design/Header/clusterdialog.h
@@ -0,0 +1,27 @@
+﻿#ifndef CLUSTERDIALOG_H
+#define CLUSTERDIALOG_H
+
+#include <QDialog>
+#include <mainwindow.h>
+
+namespace Ui {
+class clusterDialog;
+}
+
+class clusterDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit clusterDialog(QWidget *parent = nullptr);
+    ~clusterDialog();
+
+    MainWindow *p;
+private slots:
+    void on_buttonBox_accepted();
+
+private:
+    Ui::clusterDialog *ui;
+};
+
+#endif // CLUSTERDIALOG_H
